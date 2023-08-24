d9c24bc074340726926623dcaa49f00849f02de2
thiagosir <thiagobento9@hotmail.com>
2023-08-24 12:51:32 -0300

    Testando o carrinho

---
+++ carrinho/test/carrinho.test.js
@@ -0,0 +1,31 @@
+import Carrinho from "../carrinho.js";
+import Item from "../item.js";
+
+describe('Carrinhos', () => {
+    it('Deve inicializar vazio', () => {
+        const carrinho = new Carrinho();
+
+        expect(carrinho.subtotal).toBeNull();
+    });
+
+    it('Deve ter itens', () => {
+        const item = new Item('banana', 5, 5)
+        const item2 = new Item('uva', 5, 5)
+        const carrinho = new Carrinho();
+
+        carrinho.adiciona(item);
+        carrinho.adiciona(item2);
+
+        expect(typeof carrinho).toBe('object');
+        expect(carrinho.itens[0]).toBe(item);
+        expect(carrinho.itens[1]).toBe(item2);
+    })
+
+    it('Deve dar erro ao finalizar compra com carrinho vazio', () => {
+        function trataErro() {
+            const carrinho = new Carrinho();
+            carrinho.finalizaCompra();
+        }
+        expect(trataErro).toThrowError('Carrinho de compras vazio')
+    })
+})
--- carrinho/test/item.test.js
+++ carrinho/test/item.test.js
@@ -2,8 +2,18 @@ import Item from "../item.js";
 
 describe('Teste dos itens', () => {
 
-    it('Teste1', () => {
-        expect(Item).not.toBeNull();
-    })
+    it('Deve ter 3 campos: nome, valor e quantidade', () => {
 
+        const item = new Item('Beterraba', 2.5, 5);
+        
+        expect(item.nome).toBe('Beterraba')
+        expect(item.valor).toBe(2.5)
+        expect(item.quantidade).toBe(5)
+        expect(item).not.toBeNull();
+    })
+    it('Deve retornar o valor correto de acordo com a quantidade de itens', () => {
+        const item = new Item('Beterraba', 2.5, 5);
+
+        expect(item.pegaValorTotalItem()).toBe(12.5)
+    })
 })
